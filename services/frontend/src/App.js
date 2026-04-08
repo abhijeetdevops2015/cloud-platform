faebbdf8a5d1c56dd146d651bde44baf16679f86
Abhijeet <abhijeet.devops2015@gmail.com>
2026-04-07 20:56:52 -0500

    updaed

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   const callBackend = async () => {
     try {
-      const res = await fetch("http://ab54b2798f6ed4db4b1c000159a36c66-266564533.us-east-1.elb.amazonaws.com/db");
+      const res = await fetch("http://localhost:5000/db");
       const data = await res.json();
       alert(JSON.stringify(data));
     } catch (err) {
