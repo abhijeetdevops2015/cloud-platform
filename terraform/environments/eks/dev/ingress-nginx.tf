ff9851c7a4ae1f42f703926e521308d747187af9
Abhijeet <abhijeet.devops2015@gmail.com>
2026-04-10 19:27:08 -0500

    align ingress via claude

---
+++ terraform/environments/eks/dev/ingress-nginx.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "nginx_ingress" {
+  name             = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  values = [
+    <<EOF
+controller:
+  service:
+    type: LoadBalancer
+EOF
+  ]
+}
